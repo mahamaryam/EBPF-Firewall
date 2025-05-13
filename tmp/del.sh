049569eb798e501487f62dcc9c2d12c05151dcdd
maha <119899393+mahamaryam@users.noreply.github.com>
2025-05-14 03:14:24 +0500

    delete maps

---
+++ tmp/del.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo rm /sys/fs/bpf/black_protocol
+sudo rm /sys/fs/bpf/blocked_ips
+sudo rm /sys/fs/bpf/conn_table
+sudo rm /sys/fs/bpf/current_packets
+sudo rm /sys/fs/bpf/fixed_window
+sudo rm /sys/fs/bpf/fixed_window_per_ip
+sudo rm /sys/fs/bpf/ip_blocklist
+sudo rm /sys/fs/bpf/port_blocker
+sudo rm /sys/fs/bpf/main
+sudo rm /sys/fs/bpf/options
